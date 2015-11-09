1ad99bb15b83c7381f133444399cdced45c382f5
jane <enikuliak@gmail.com>
2015-11-08 16:30:10 -0800

    First files for Maven projects

---
+++ src/main/java/com/jane/App.java
@@ -0,0 +1,10 @@
+package com.jane;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World, Evgenia Nikuliak.");
+    }
+}
